be35513e0011c0ef485ecf6676c73349c0bd7e4e
RD-developer1 <rahamdilpitafi03@gmail.com>
2024-06-11 17:18:44 +0500

    day2 nowCompltd

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class homePage extends StatelessWidget {
   const homePage({super.key});
-
+      final int i=0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome"),
+          ),
         body: Center(
-          child: Text('Hello World!'),
-        ),
+          child: Text('Rahamdil $i'),),
+          drawer: Drawer(),
       );
   }
 }
